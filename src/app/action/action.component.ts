8b8c515da6735eebfccc991c7397f8b5db8e7797
iwamih31 <iwamih31@gmail.com>
2024-06-26 22:11:23 +0900

    action.component修正

--- src/app/action/action.component.ts
+++ src/app/action/action.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { NgFor, AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-action',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink, AsyncPipe],
   templateUrl: './action.component.html',
   styleUrl: './action.component.css'
 })
-export class ActionComponent {
+export class ActionComponent implements OnInit {
+  users$!: Observable<User[]>;
+  private searchTerms = new Subject<string>();
 
+  constructor(private userService: UserService) {}
+
+  // 検索語をobservableストリームにpushする
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.users$ = this.searchTerms.pipe(
+      // 各キーストロークの後、検索前に300ms待つ
+      debounceTime(300),
+
+      // 直前の検索語と同じ場合は無視する
+      distinctUntilChanged(),
+
+      // 検索語が変わる度に、新しい検索observableにスイッチする
+      switchMap((term: string) => this.userService.searchUsers(term)),
+    );
+  }
 }
