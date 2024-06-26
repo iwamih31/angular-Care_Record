c243e7e3a3eaf1b577121d639143e5ecf8586b63
iwamih31 <iwamih31@gmail.com>
2024-06-26 22:39:50 +0900

    app.routes.ts修正

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { ActionComponent } from './action/action.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: 'users', component: UsersComponent },
 	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'detail/:id', component: UserDetailComponent },
+	{ path: 'action/:id', component:ActionComponent },
 ];
