81179b7f3246a93ae4456bbf40dd64a8aa36a5d9
iwamih31 <iwamih31@gmail.com>
2024-06-30 01:46:29 +0900

    routine.component.ts修正

--- src/app/routine/routine.component.ts
+++ src/app/routine/routine.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-routine',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+    RouterModule,
+  ],
   templateUrl: './routine.component.html',
   styleUrl: './routine.component.css'
 })
 export class RoutineComponent {
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+    .subscribe(users => this.users = users);
+  }
+
+  add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.userService.addUser({ name } as User)
+    .subscribe(user => {
+      this.users.push(user);
+    });
+  }
+
+  delete(user: User): void {
+    this.users = this.users.filter(h => h !== user);
+    this.userService.deleteUser(user.id).subscribe();
+  }
 
 }
