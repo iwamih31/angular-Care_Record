c415f1801b4193b70997c407f2ac3cf246b45848
iwamih31 <iwamih31@gmail.com>
2024-06-30 17:41:37 +0900

    app.routes.ts修正

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { UsersComponent } from './users/users.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { ActionComponent } from './action/action.component';
+import { RoutineComponent } from './routine/routine.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: 'users', component: UsersComponent },
+	{ path: 'routine', component: RoutineComponent },
 	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'detail/:id', component: UserDetailComponent },
 	{ path: 'action/:userId', component:ActionComponent },
