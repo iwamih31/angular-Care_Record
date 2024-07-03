7ac3e34bcf4b15b46fc5751a7f22e53d8498884a
iwamih31 <iwamih31@gmail.com>
2024-07-03 23:27:35 +0900

    todo.component作成

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+}
