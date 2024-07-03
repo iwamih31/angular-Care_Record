311127d11a9cdb854bf8e8f95473e3a8b7b76099
iwamih31 <iwamih31@gmail.com>
2024-07-03 23:52:34 +0900

    todo.component.ts修正

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { NgIf, UpperCasePipe, Location,} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+
+
+import {User} from '../user';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+    imports: [
+    NgIf,
+    UpperCasePipe,
+    FormsModule,
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class ToDoComponent {
+  @Input() user?: User;
 
+  constructor(
+		private readonly route: ActivatedRoute,
+		private userService: UserService,
+		private location: Location,
+	) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.userService.getUser(id)
+      .subscribe(user => this.user = user);
+  }
+
+	goBack(): void {
+		this.location.back();
+	}
+
+	save(): void {
+    if (this.user) {
+      this.userService.updateUser(this.user)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
