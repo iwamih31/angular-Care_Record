0df7d3219ce26ce53459dd7d2c38015f7a0b6e01
iwamih31 <iwamih31@gmail.com>
2024-07-12 00:05:57 +0900

    in-memory-data.service.ts修正

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -35,11 +35,11 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 3, action: '水分', note: '朝一の水分補給', date: '予定', time: '08:30', userId: 0 },
       { id: 4, action: '朝食', note: '食事の状態確認', date: '予定', time: '09:00', userId: 0 },
       { id: 5, action: '体操', note: '体操内容確認', date: '予定', time: '10:00', userId: 0 },
-      { id: 6, action: '昼食', note: 'ト中', date: '予定', time: '12:00', userId: 0 },
-      { id: 7, action: '運動', note: '20', date: '予定', time: '14:30', userId: 0 },
-      { id: 8, action: 'おやつ', note: 'ト普少', date: '予定', time: '15:00', userId: 0 },
-      { id: 9, action: '夕食', note: '40', date: '予定', time: '17:30', userId: 0 },
-      { id: 10, action: '就寝', note: '40', date: '予定', time: '19:30', userId: 0 }
+      { id: 6, action: '昼食', note: '食事の状態確認', date: '予定', time: '12:00', userId: 0 },
+      { id: 7, action: '運動', note: '運動の様子確認', date: '予定', time: '14:30', userId: 0 },
+      { id: 8, action: 'おやつ', note: '食事の状態確認', date: '予定', time: '15:00', userId: 0 },
+      { id: 9, action: '夕食', note: '食事の状態確認', date: '予定', time: '17:30', userId: 0 },
+      { id: 10, action: '就寝', note: '消灯した時刻', date: '予定', time: '19:30', userId: 0 }
     ];
     return {users, actions, routine};
   }
